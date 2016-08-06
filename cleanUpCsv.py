34f5489e85bbb39b9ea3f4bd1cb5edbcc57027ef
Mike Sherry <mikesherry24@gmail.com>
2016-08-06 16:21:28 -0500

    clean up csv output to a workable format

--- cleanUpCsv.py
+++ cleanUpCsv.py
@@ -3,20 +3,106 @@ from os.path import isfile, join, dirname
 import csv
 
 def processRow(row):
-	line2 = row['line2']
-	line2Array = line2.split(' ')
-	line2Array.remove()
-	print line2Array
+	name = getName(row)
+	hours = getHours(row)
+	salary = getSalary(row)
+	title = getTitle(row)
+	
+	return {
+		'name': name,
+		'hours': hours,
+		'salary1': salary[0],
+		'salary2': salary[1],
+		'salary3': salary[2],
+		'title': title
+	}
 
+def getTitle(row):
+	lineAsArray = getLineAsArrayOfStrings(row, 'line3')
+	wordsInTitle = []
+	for word in lineAsArray:
+		if not IsInt(word):
+			wordsInTitle.append(word)
+	title =  ' '.join(wordsInTitle)
+	return title
+
+def getName(row):
+	lineAsArray = getLineAsArrayOfStrings(row, 'line1')
+	wordsInName = []
+	for word in lineAsArray:
+		if not IsInt(word):
+			wordsInName.append(word)
+	name =  ' '.join(wordsInName)
+	return name
+
+def getHours(row):
+	lineAsArray = getLineAsArrayOfStrings(row, 'line1')
+	for word in lineAsArray:
+		if IsInt(word):
+			return word
+	return ''
+
+def getSalary(row):
+	lineAsArray = getLineAsArrayOfStrings(row, 'line2')
+	salaries = []
+	for word in lineAsArray:
+		wordWithoutCommas = removeCommas(word)
+		if IsInt(wordWithoutCommas):
+			salaries.append(wordWithoutCommas)
+	formattedSalaries = createOneByThreeArray(salaries)
+	return formattedSalaries
+
+def createOneByThreeArray(inputArray):
+	while len(inputArray)  < 3:
+		inputArray.append('0')
+	return inputArray
+
+def getLineAsArrayOfStrings(row, line):
+	targetLine = row[line]
+	return targetLine.split(' ')
+
+def removeCommas(word):
+	return word.replace(',', '')
+
+def IsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def writeRowsToFile(rows, filename):
+	with open(getFormattedCsvFilePath(filename), "w") as csvfile:
+		fieldNames = ['name', 'title', 'hours', 'salary1', 'salary2', 'salary3']
+		out = csv.DictWriter(csvfile, fieldNames)
+		out.writeheader()
+		for row in rows:
+			out.writerow({
+				'name' : row['name'],
+				'title' : row['title'],
+				'hours' : row['hours'],
+				'salary1' : row['salary1'],
+				'salary2' : row['salary2'],
+				'salary3' : row['salary3']
+			})
+
+def getFormattedCsvFilePath(file):
+	return 'formattedCsv/' + file
 
 
 csvPath = "./csv"
 csvFiles = [f for f in listdir(csvPath) if isfile(join(csvPath, f)) and '.csv' in f]
 print csvFiles
 
+
 for file in csvFiles:
+	rows = []
 	with open('csv/' + file) as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-		    processRow(row)
+		    rowObject = processRow(row)
+		    rows.append(rowObject)
+		writeRowsToFile(rows, file)
+
+
 
